a229f5d8bab74b701f4618a42d65c92d60b5cce1
rohanvron <164373071+rohanvron@users.noreply.github.com>
2024-05-01 02:28:55 +0530

    Update: AgeDisplay.jsx

--- src/components/AgeDisplay.jsx
+++ src/components/AgeDisplay.jsx
@@ -1,19 +1,24 @@
 import { useSpring, animated } from "@react-spring/web";
 
-const AgeDisplay = ({label, value }) => {
-    const isValueZero = value === 0;
+const AgeDisplay = ({ label, value }) => {
+  const isValueZero = value === 0;
+  const formattedValue = value < 10 && value !== 0 ? `0${value}` : value;
+  const displayValue = value === 0 ? "--" : formattedValue;
 
-    // spring animation
-    const props = useSpring({number: value, from: {number: 0} });
+  // spring animation
+  const props = useSpring({ number: value, from: { number: 0 } });
 
   return (
     <h2>
-    {isValueZero ? (<span className="text-purple">--</span>) : 
-                    (<animated.span className="text-purple">
-                        {props.number.to((n) => Math.floor(n))}
-                    </animated.span>)}
-                    {" "}
-        {label}
+      {isValueZero ? (
+        <span className="text-purple">{displayValue}</span>
+      ) : (
+        <animated.span className="text-purple">
+          {props.number.to((n) => (n < 10 && n !== 0 ? `0${Math.floor(n)}` : Math.floor(n)))}
+        </animated.span>
+      )}
+      {" "}
+      {label}
     </h2>
   );
 };
